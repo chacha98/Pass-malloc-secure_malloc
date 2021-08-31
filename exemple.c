323755e2bdf5274ff9b378c240b0744b3e8bda2e
YANHAMO Charnelle <kapawouchanelle@gmail.com>
2021-08-31 22:33:10 +0100

    new commit: update pass

--- exemple.c
+++ exemple.c
@@ -1,12 +1,24 @@
 #include <stdlib.h>
 #include <stdio.h>
 
-int main(){
-int *ptr;
-ptr = malloc(15); //a block of 15 integers 
-    if (ptr != NULL) {
-      *(ptr + 5) = 480; // assign 480 to sixth integer 
-      printf("Value of the 6th integer is %d",*(ptr + 5));
-    }
+void hello(){
+	printf("hello word\n");
+}
 
+void call_malloc(){
+	int *ptr;
+	ptr = malloc(15); //a block of 15 integers 
+
+    	if (ptr != NULL) {
+      		*(ptr + 5) = 480; // assign 480 to sixth integer 
+      		printf("Value of the 6th integer is %d",*(ptr + 5));
+    	}
+}
+
+int main(){
+	hello();
+	call_malloc();
+	
+    	
+	
 }
--- pass-malloc-secure_malloc/pass.cpp
+++ pass-malloc-secure_malloc/pass.cpp
@@ -6,7 +6,9 @@
 #include "llvm/Transforms/IPO/PassManagerBuilder.h"
 #include "llvm/IR/IRBuilder.h"
 #include "llvm/Transforms/Utils/BasicBlockUtils.h"
+#include <llvm/Support/raw_ostream.h>
 using namespace llvm;
+
 
 
 namespace {
@@ -15,63 +17,44 @@ namespace {
     Malloc_to_secure_mallocPass() : FunctionPass(ID) {}
 
     virtual bool runOnFunction(Function &F) {
-      //errs() << "I saw a function called " << F.getName() << "!\n";
-      /*errs() << "Function body:\n" << F << "\n";
-      for (auto& B : F) {
-  	errs() << "Basic block:\n" << B << "\n";
-  	for (auto& I : B) {
-    		errs() << "Instruction: " << I << "\n";
-  	}
-	}*/
-
+  	
 	for (auto& B : F) {
-  		for (auto& I : B) {
-	
-  		if(CallInst* call_inst = dyn_cast<CallInst>(&I)) {
-                Function* fn = call_inst->getCalledFunction();
-                StringRef fn_name = fn->getName();
-                //errs() << fn_name << " : " << fn_name->arg_begin()<<"\n";
-                errs() << fn_name << " : " << call_inst->getArgOperand(0) << "\n";
-                for(auto arg = fn->arg_begin(); arg != fn->arg_end(); ++arg) {
-  		if(auto* ci = dyn_cast<ConstantInt>(arg))
-    		errs() << ci->getValue() << "\n";
-  		errs() << *arg << "\n";
-		}
-                return false;
-                }
-    			/*if (auto* op = dyn_cast<BinaryOperator>(&I)) {
-      		// Insert at the point where the instruction `op` appears.
-      			IRBuilder<> builder(op);
-
-      			// Make a multiply with the same operands as `op`.
-     			 Value* lhs = op->getOperand(0);
-      			 Value* rhs = op->getOperand(1);
-      			 Value* mul = builder.CreateMul(lhs, rhs);
-
-      			// Everywhere the old instruction was used as an operand, use our
-      			// new multiply instruction instead.
-      			for (auto& U : op->uses()) {
-        			User* user = U.getUser();  // A User is anything with operands.
-        			user->setOperand(U.getOperandNo(), mul);
-      			}
-
-     			 // We modified the code.
-     			 return true;
-   			 }*/
+  		for (auto& I : B) {		
+  			if(CallInst* call_inst = dyn_cast<CallInst>(&I)) {  // on vérifie si l'instruction est de type appel de fonction
+  		
+                		Function* fn = call_inst->getCalledFunction();   //on récupère la fonction appelée
+                		StringRef fn_name = fn->getName();              // on récupère le nom de ladite fonction
+                		if (fn_name=="malloc"){                       // si c'est un appel malloc
+                			
+                			// on affiche le nom de la fonction et la
+                			errs() << "function name : " << fn_name <<"\n"; 
+                			errs() << "size of allocation : " << *(call_inst->getOperand(0)) <<"\n";      
+                		}
+                		
+               		/* for(auto arg = fn->arg_begin(); arg != fn->arg_end(); ++arg) {
+  				if(auto* ci = dyn_cast<ConstantInt>(arg))
+    				errs() << ci->getValue() << "\n";
+  				errs() << *arg << "\n";
+				}*/
+			
+				// on retourne faux car on a pas encore modifié notre programme
+                		return false;
+                	}
+    	
   		}
 	}	
-}
-};
+    }
+ };
 }
 
 char Malloc_to_secure_mallocPass::ID = 0;
 
 // Automatically enable the pass.
 // http://adriansampson.net/blog/clangpass.html
-static void registerSkeletonPass(const PassManagerBuilder &,
+static void registerMalloc_to_secure_mallocPass(const PassManagerBuilder &,
                          legacy::PassManagerBase &PM) {
   PM.add(new Malloc_to_secure_mallocPass());
 }
 static RegisterStandardPasses
   RegisterMyPass(PassManagerBuilder::EP_EarlyAsPossible,
-                 registerSkeletonPass);
+                 registerMalloc_to_secure_mallocPass);
